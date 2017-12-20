1684a49b85328481a33a43fb3c275e5c2f660b21
Anthony Penalosa <aj@anthonys-mbp.attlocal.net>
2017-12-20 03:57:42 -0800

    Added more questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 
     var questions = [
         {
+            // Question 1
             question: "What is Doc's dog's name?",
             correctAnswer: "Einstein",
             optionB: "Darwin",
@@ -19,6 +20,7 @@ $(document).ready(function() {
             optionD: "Newton" 
         },
         {
+            // Question 2
             question: "How many gigawatts does the DeLorean need to make time travel possible?",
             optionA: "1.41",
             correctAnswer: "1.21",
@@ -26,11 +28,68 @@ $(document).ready(function() {
             optionD: "1.51"
         },
         {
+            // Question 3
             question: "Who was originally cast as Marty McFly before Michael J. Fox was recast?",
             optionA: "John Cusack",
             optionB: "Rob Lowe",
             correctAnswer: "Eric Stoltz",
             optionD: "Matthew Broderick"
+        },
+        {
+            // Question 4
+            question: "What kind of fuel does the DeLorean time machine use?",
+            optionA: "Kerosene",
+            correctAnswer: "Plutonium",
+            optionC: "Diesel",
+            optionD: "Uranium"
+        },
+        {
+            // Question 5
+            question: "What time is it when lightning strikes the clock tower?",
+            correctAnswer: "10:04 PM",
+            optionB: "10:01 PM",
+            optionC: "10:05 PM",
+            optionD: "10:11 PM"
+        },
+        {
+            // Question 6
+            question: "Finish the line: \"I guess you guys aren't ready for that yet...\"",
+            correctAnswer: "But your kids are gonna love it.",
+            optionB: "But you will be someday.",
+            optionC: "But that's cool.",
+            optionD: "But you'll get there."
+        },
+        {
+            // Question 7
+            question: "What is Marty's band called?",
+            optionA: "The Hammers",
+            correctAnswer: "The Pinheads",
+            optionC: "The Pee Wees",
+            optionD: "Johnny B. Goode"
+        },
+        {
+            // Question 8
+            question: "What name did Marty go by in 1955?",
+            correctAnswer: "Calvin Klein",
+            optionB: "Hugo Boss",
+            optionC: "John Kennedy",
+            optionD: "Tom Cruise"
+        },
+        {
+            // Question 9
+            question: "Finish the line: \"Why don't you make like a tree and...\"",
+            optionA: "Blow in the wind.",
+            optionB: "Leave.",
+            optionC: "Fall down.",
+            correctAnswer: "Get out of here."
+        },
+        {
+            // Question 10
+            question: "What is Marty's girlfriend's name?",
+            optionA: "Marilyn",
+            optionB: "Allison",
+            correctAnswer: "Jennifer",
+            optionD: "Anna"
         }
     ];
 
