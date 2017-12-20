7edb09efe846ae774fd38c9f1d23927e43917659
Anthony Penalosa <aj@anthonys-mbp.attlocal.net>
2017-12-20 00:09:11 -0800

    Creating game function Moving objects into an array Creating reference to .questions-wrapper Creating a loop through the properties of the first objec in the array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,31 +9,29 @@ $(document).ready(function() {
 
     // Questions
 
-    var question01 = {
-        question: "What is Doc's dog's name?",
-        optionA: "Einstein",
-        optionB: "Darwin",
-        optionC: "Edison",
-        optionD: "Newton"
-    };
-
-    var question02 = {
-        question: "How many gigawatts does the DeLorean need to make time travel possible?",
-        optionA: "1.41",
-        optionB: "1.21",
-        optionC: "1.31",
-        optionD: "1.51"
-    };
-
-    var question03 = {
-        question: "Who was originally cast as Marty McFly before Michael J. Fox was recast?",
-        optionA: "John Cusack",
-        optionB: "Rob Lowe",
-        optionC: "Eric Stoltz",
-        optionD: "Matthew Broderick"
-    };
-
-    var questions = [question01, question02, question03];
+    var questions = [
+        {
+            question: "What is Doc's dog's name?",
+            correctAnswer: "Einstein",
+            optionB: "Darwin",
+            optionC: "Edison",
+            optionD: "Newton" 
+        },
+        {
+            question: "How many gigawatts does the DeLorean need to make time travel possible?",
+            optionA: "1.41",
+            correctAnswer: "1.21",
+            optionC: "1.31",
+            optionD: "1.51"
+        },
+        {
+            question: "Who was originally cast as Marty McFly before Michael J. Fox was recast?",
+            optionA: "John Cusack",
+            optionB: "Rob Lowe",
+            correctAnswer: "Eric Stoltz",
+            optionD: "Matthew Broderick"
+        }
+    ];
 
     function timeUp() {
 
@@ -41,9 +39,37 @@ $(document).ready(function() {
 
     function newGame() {
 
-        alert("NEW GAME!!!");
+        // Creating reference to .questions-wrapper
+        var questionsWrapper = $(".questions-wrapper");
+            
+        // Variables for the iterations
+        var questionNumber = 0;
+        var questionItem;
+        var isFirstItem = true;
 
+        // Iterates through the properties of each question in the questions array
+        for(var questionProperty in questions[questionNumber]) {
 
+            // Variable to hold the contents of each property
+            var questionItemContent = questions[questionNumber][questionProperty];
+
+            if (isFirstItem) {
+                // Puts first property in a new p element
+                questionItem = $("<h3>").text(questionItemContent);
+                isFirstItem = false;
+            }
+
+            else {
+                // Puts the rest of the properties in anchor tags
+                questionItem = $("<a>").text(questionItemContent).addClass("question-option");
+            }
+    
+            // Appends the p element to the questions wrapper
+            questionsWrapper.append(questionItem);
+
+            console.log(questionItemContent);
+
+          }
     }
 
     $(".start-button").on("click", function() {
